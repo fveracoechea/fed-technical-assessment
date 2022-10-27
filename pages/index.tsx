5e862d72142d151536940533b0797d0246b306e6
Michael Zipp <milonti1@gmail.com>
2022-10-27 14:15:58 -0400

    Upgraded index to have loading/loaded/error states

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,31 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import ProductDetails from "components/ProductDetails";
 import ProductVariations from "components/ProductVariations";
+import { useEffect, useState } from "react";
 
 const title = "RTG Frontend technical assessment";
 
-const PDP: NextPage = () => (
+
+const PDP: NextPage = () => {
+  
+  const [product, setProduct] = useState(null);
+
+  useEffect( () => {
+    const url = "/api/product"
+    const fetchProduct = async () => {
+      try {
+        const res = await fetch(url);
+        const json = await res.json();
+        setProduct(json);
+      }
+      catch ( err) {
+        console.error(err);
+      }
+    }
+    fetchProduct();
+  }, []) //Should run once on first mount
+
+  return (
   <div className="container">
     <Head>
       <title>{title}</title>
@@ -14,12 +35,26 @@ const PDP: NextPage = () => (
 
     <main className="main">
       <h1>{title}</h1>
-      <section className="product">
-        <ProductDetails />
-        <ProductVariations />
-      </section>
+      {product ? ( //If null has not loaded
+        product.errorMessage ? ( //All errors should have error message if coming from the api
+          <section>
+            <div>Product not found, please try again...</div>
+          </section>
+        ) : (
+          <section className="product">
+            <div>{product.title}</div>
+            <ProductDetails />
+            <ProductVariations />
+          </section>
+        )
+      ) : (
+        <section className="product">
+          <div>Loading...</div>
+        </section>
+      )}
+
     </main>
   </div>
-);
+)};
 
 export default PDP;
